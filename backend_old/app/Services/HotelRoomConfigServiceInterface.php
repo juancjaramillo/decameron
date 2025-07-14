816df6d395f1c7a5eb9ff8245ae9d0c7149c9855
Juan Carlos Jaramillo <ingenierojramillo@gmail.com>
2025-07-13 19:36:05 -0500

    Inicial: backend + frontend

--- backend_old/app/Services/HotelRoomConfigServiceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Services;
-
-interface HotelRoomConfigServiceInterface
-{
-    public function listar(int $hotelId): array;
-    public function crear(int $hotelId, array $data): array;
-    public function eliminar(int $id): void;
-}
